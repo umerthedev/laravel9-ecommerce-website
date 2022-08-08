c0ab093570430e50ae68f43f3ed003703a2e1dad
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-08-09 00:59:51 +0600

    admin and user dashboard design with dynamic login

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,34 @@
-<x-app-layout>
+{{-- <x-app-layout>
 
-</x-app-layout>
+</x-app-layout> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      @include('admin.header')
+        <!-- partial -->
+        @include('admin.body')
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Copyright <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Isdb Students</a> from Umer</span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    @include('admin.script')
+  </body>
+</html>
