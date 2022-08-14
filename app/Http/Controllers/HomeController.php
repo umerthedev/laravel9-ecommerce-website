f98a6f77c4deebb272485f8ddc4cb8d65c3bfb82
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-08-14 15:53:10 +0600

    pagination added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\product;
 
 class HomeController extends Controller
 {
-        // index
+    // index
     public function index()
     {
-        return view('home.userpage');
+        $product = product::paginate(10);
+        return view('home.userpage', compact('product'));
     }
 
 
@@ -19,15 +21,11 @@ class HomeController extends Controller
     public function redirect()
     {
         $usertype = Auth::user()->usertype;
-        if($usertype == '1')
-        {
+        if ($usertype == '1') {
             return view('admin.home');
-        }
-        else
-        {
-            return view('home.userpage');
+        } else {
+            $product = product::paginate(10);
+            return view('home.userpage', compact('product'));
         }
     }
-
-    
 }
