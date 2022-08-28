4b2646f2b591597f4d2e8540bdb78f82377918ae
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-08-29 00:23:02 +0600

    product comment section Added

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -2,6 +2,7 @@
 <html>
    <head>
      @include('home.css')
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
    </head>
    <body>
 
@@ -26,6 +27,61 @@
       @include('home.product')
       <!-- end product section -->
 
+      <!-- Comments and replies section Start -->
+      
+   <div style="text-align: center;padding-bottom:30px">
+         <h1 style="font-size:30px; text-align:center; padding-top:20px; padding-bottom:20px">Comments</h1>
+      <form action="">
+         <textarea style="height:150px; width:600px; text-align:center;" name="" placeholder="Your comments here"></textarea>
+         <br>
+        <button  style="pa" class="btn btn-primary text-dark" type="submit" name="submit" value="Submit">Submit</button>
+      </form>
+   </div>
+   <div style="padding-left:20%">
+      <h1 style="font-size:30px;padding-bottom:30px">All Comments</h1>
+      <div style="margin-top:10px">
+         <b>Umer</b>
+         <p>This is my first comment</p>
+         <a href="javascript:void(0)"oneclick="reply(this);">Reply</a>
+      </div>
+      <div style="margin-top:10px">
+         <b>Umer</b>
+         <p>This is my first comment</p>
+         <a href="javascript:void(0);" oneclick="reply(this);">Reply</a>
+      </div>
+      <div style="margin-top:10px">
+         <b>Umer</b>
+         <p>This is my first comment</p>
+         <a href="javascript:void(0);" oneclick="reply(this);">Reply</a>
+         
+      </div>
+      <div style="display:none" class="replyDiv">
+         <textarea name="" style="height: 100px; width: 500px" placeholder="Write Your Comments Here"></textarea> <br>
+         <a href="" class="btn btn-primary" style="margin-top: 10px; margin-bottom: 10px;">Reply</a>
+      </div>
+   </div>
+  
+
+
+
+
+
+
+
+
+
+
+      <!-- Comments and replies section End -->
+
+
+
+
+
+
+
+
+
+
       <!-- subscribe section -->
       @include('home.subscribe')
       <!-- end subscribe section -->
@@ -45,6 +101,13 @@
          
          </p>
       </div>
+      <script>
+         function reply(caller) {
+            $('.replyDiv').insertAfter($(caller));
+            $('.replyDiv').show();
+         }
+      </script>
       @include('home.script');
+      
    </body>
 </html>
